8902acc521b2beaa504cbf4d54f749d496430734
Kateryna Stepurska <katerynastepurska@Katerynas-MacBook-Pro.local>
2023-11-06 13:06:12 -0800

    added login

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import hashlib
+import os
 
 app = Flask(__name__)
+#app.secret_key = 'your_secret_key'
 
 @app.route('/form')
 def form():
@@ -14,6 +17,10 @@ def index():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -42,6 +49,36 @@ def submit():
         conn.close()
 
         return redirect(url_for('form'))
+    
+#login
+
+def hash_password(password, salt):
+    hash_obj = hashlib.sha256(salt.encode() + password.encode())
+    return hash_obj.hexdigest()
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    #username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    role = 'user'  # Assign a default role, e.g., 'user'
+
+
+    # Generate a random salt for each user (store it in the database)
+    salt = os.urandom(16).hex()
+    hashed_password = hash_password(password, salt)
+
+    conn = sqlite3.connect('user_database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('INSERT INTO users ( email, password, role) VALUES (?, ?, ?)', ( email, hashed_password, role))
+  
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('login'))
+###
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ user_database.py
@@ -0,0 +1,29 @@
+import sqlite3
+import hashlib
+import os
+
+# Define a function to create the SQLite database and user table
+def create_database():
+    # Check if the database file exists; if not, create it
+    if not os.path.exists('user_database.db'):
+        conn = sqlite3.connect('user_database.db')
+        cursor = conn.cursor()
+
+        # Create a user table with fields for username, email, and hashed password   
+        cursor.execute('''         
+            CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL,
+                role TEXT NOT NULL
+            )
+        ''')
+
+        conn.commit()
+        conn.close()
+        print("Database created successfully.")
+    else:
+        print("Database already exists.")
+
+# Call the function to create the database and user table
+create_database()
