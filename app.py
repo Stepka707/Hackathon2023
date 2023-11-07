609b20f3e31620804ca6aa1da068f3eecaf5d415
Kateryna Stepurska <katerynastepurska@Katerynas-MacBook-Pro.local>
2023-11-07 11:16:36 -0800

    added protected pages

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session,flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+
 
 import sqlite3
 import hashlib
@@ -9,46 +11,109 @@ import bcrypt
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+#---------POSIBLE CAN BE DELETED----------
+# Configure Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, email):
+        self.email = email
+
+    def get_id(self):
+        return self.email
+
+login_manager = LoginManager(app)
+
+# Define a function to query the database and load a user by email
+@login_manager.user_loader
+def load_user(email):
+    connection = sqlite3.connect('user_database.db')  
+    cursor = connection.cursor()
+    cursor.execute("SELECT email FROM users WHERE email = ?", (email,))
+    user_data = cursor.fetchone()
+    connection.close()
+
+    if user_data:
+        return User(user_data[0])  # Create a User instance with the email
+    else:
+        return None  # User not found
+#---------------------------------
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    # Check if the user is logged in using the session
+    if 'user_username' in session:
+        email = session['user_username']
+
+        # Render the dashboard with user-specific data
+        return render_template('contact.html', email=email)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
+@app.route('/protected')
+def protected():
+    # Check if the user is logged in using the session
+    if 'user_username' in session:
+        email = session['user_username']
+
+        # Render the dashboard with user-specific data
+        return render_template('protected.html', email=email)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/protected_home')
+def protected_home():
+    # Check if the user is logged in using the session
+    if 'user_username' in session:
+        email = session['user_username']
+
+        # Render the dashboard with user-specific data
+        return render_template('protected_home.html', email=email)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    # Clear the session to log the user out
+    session.clear()
+    return redirect(url_for('login'))
+
+#@app.route('/submit', methods=['POST'])
+#def submit():
+ #  if request.method == 'POST':
+   #     name = request.form['name']
+   #     email = request.form['email']
+   #     message = request.form['message']
 
         # Connect to the SQLite database
-        conn = sqlite3.connect('mydatabase.db')
-        cursor = conn.cursor()
+     #   conn = sqlite3.connect('mydatabase.db')
+     #   cursor = conn.cursor()
 
         # Create a table if it doesn't exist
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS contacts (
-                name TEXT,
-                email TEXT,
-                message TEXT
-            )
-        ''')
+       # cursor.execute('''
+        #    CREATE TABLE IF NOT EXISTS contacts (
+         #       name TEXT,
+          #      email TEXT,
+          #      message TEXT
+          #  )
+       # ''')
 
         # Insert data into the table
-        cursor.execute('INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)', (name, email, message))
+       # cursor.execute('INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)', (name, email, message))
 
-        conn.commit()
-        conn.close()
+       # conn.commit()
+       # conn.close()
 
-        return redirect(url_for('form'))
+       # return redirect(url_for('form'))
 
 #--------------FUNCTIONS--------------
 
@@ -76,6 +141,7 @@ def userlogin():
 
         # Perform user authentication here
         if user_is_authenticated(email, password):
+
             # Create a session for the user
             session['user_username'] = email
             
@@ -117,24 +183,6 @@ def user_is_authenticated(email, password):
                 return True  # Authentication successful
 
     return False  # Authentication failed
-
-@app.route('/protected')
-def protected():
-    # Check if the user is logged in using the session
-    if 'user_username' in session:
-        email = session['user_username']
-
-        # Render the dashboard with user-specific data
-        return render_template('protected.html', email=email)
-    else:
-        return redirect(url_for('login'))
-
-@app.route('/logout')
-def logout():
-    # Clear the session to log the user out
-    session.clear()
-    return redirect(url_for('login'))
-    
 #---------------------------------
 
 #-------Registration--------------
